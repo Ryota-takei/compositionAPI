feef742ce5d07619b2e0b6a8213fa0f43b1935bd
Ryota-takei <wlol0747@yahoo.co.jp>
2021-07-31 13:59:00 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Buefy from "buefy";
+import VueCompositionApi from "@vue/composition-api";
+import "buefy/dist/buefy.css";
 
-Vue.config.productionTip = false
+Vue.use(Buefy);
+Vue.use(VueCompositionApi);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
